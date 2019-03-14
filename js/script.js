5987f7f3272b0514d7972d9e32aaed3a2e9be002
angrycrows <moonmh@mail.ru>
2019-03-14 10:42:38 +0300

    Project v1

---
+++ js/script.js
@@ -0,0 +1,214 @@
+$(document).ready(function () {
+	$("#ie-label-1").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-1").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-2").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-2").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-3").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-3").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-4").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-4").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-5").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-5").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-6").on("click", function() {
+		$(".ie__container").removeClass("ie__container__active");
+		$(".ie__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-6").addClass("ie__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-7").on("click", function() {
+		$(".ie__container__min").removeClass("ie__container__min__active");
+		$(".ie__labels__min").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-7").addClass("ie__container__min__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#ie-label-8").on("click", function() {
+		$(".ie__container__min").removeClass("ie__container__min__active");
+		$(".ie__labels__min").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#ie-content-8").addClass("ie__container__min__active");
+	})
+})
+
+// Вторая таблица
+
+$(document).ready(function () {
+	$("#oe-label-1").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-1").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-2").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-2").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-3").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-3").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-4").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-4").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-5").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-5").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-6").on("click", function() {
+		$(".oe__container").removeClass("oe__container__active");
+		$(".oe__labels").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-6").addClass("oe__container__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-7").on("click", function() {
+		$(".oe__container__min").removeClass("oe__container__min__active");
+		$(".oe__labels__min").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-7").addClass("oe__container__min__active");
+	})
+})
+
+$(document).ready(function () {
+	$("#oe-label-8").on("click", function() {
+		$(".oe__container__min").removeClass("oe__container__min__active");
+		$(".oe__labels__min").removeClass("s-4__label__active");
+		$(this).addClass("s-4__label__active");
+		$("#oe-content-8").addClass("oe__container__min__active");
+	})
+})
+
+// Преимущества
+
+$(document).ready(function() {
+	$("#advantage-1").on("mouseover", function() {
+		$(".s-2__container__point__items").removeClass("s-2__container__point__items__active");
+		$(this).parent().addClass("s-2__container__point__items__active");
+		$(".s-2__container__img").css({"backgroundImage":"url(img/interactive.png)"});
+		$(".s-2__container__desc").removeClass("s-2__container__desc__active");
+		$("#desc-1").addClass("s-2__container__desc__active");
+	})
+})
+
+$(document).ready(function() {
+	$("#advantage-2").on("mouseover", function() {
+		$(".s-2__container__point__items").removeClass("s-2__container__point__items__active");
+		$(this).parent().addClass("s-2__container__point__items__active");
+		$(".s-2__container__img").css({"backgroundImage":"url(img/interactive2.png)"});
+		$(".s-2__container__desc").removeClass("s-2__container__desc__active");
+		$("#desc-2").addClass("s-2__container__desc__active");
+	})
+})
+
+$(document).ready(function() {
+	$("#advantage-3").on("mouseover", function() {
+		$(".s-2__container__point__items").removeClass("s-2__container__point__items__active");
+		$(this).parent().addClass("s-2__container__point__items__active");
+		$(".s-2__container__img").css({"backgroundImage":"url(img/interactive3.png)"});
+		$(".s-2__container__desc").removeClass("s-2__container__desc__active");
+		$("#desc-3").addClass("s-2__container__desc__active");
+	})
+})
+
+$(document).ready(function() {
+	$("#advantage-4").on("mouseover", function() {
+		$(".s-2__container__point__items").removeClass("s-2__container__point__items__active");
+		$(this).parent().addClass("s-2__container__point__items__active");
+		$(".s-2__container__img").css({"backgroundImage":"url(img/interactive4.png)"});
+		$(".s-2__container__desc").removeClass("s-2__container__desc__active");
+		$("#desc-4").addClass("s-2__container__desc__active");
+	})
+})
+
+// Слайдер
+
+$(document).ready(function() {
+	$(".slider").slick({
+		arrows: true,
+		dots: false,
+		slidesToScroll: 1,
+		slidesToShow: 2
+	})
+})
+
+// Модальная форма
+
+$(document).ready(function() {
+	$(".hr-phone__second").on("click", function() {
+		$(".overlay__modal").show();
+		$(".overlay").fadeIn(500);
+	})
+})
+
+$(document).ready(function() {
+	$(".overlay__modal").on("click", function() {
+		$(this).hide();
+		$(".overlay").hide();
+	})
+})
